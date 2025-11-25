398f709d36523c866cd6886b36b6ab84b213deeb
Sara Sin <ssin@student.42berlin.de>
2025-11-25 18:50:42 +0100

    feat: add readline

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	char *prompt;
+
+	while (1)
+	{
+		prompt = readline("$[ 🛸 ]>");
+		//printf("%s\n", prompt);
+		//if (ft_strncmp(prompt, "exit"))
+		//	exit(EXIT_SUCCESS);
+	}
+	return (0);
+}
