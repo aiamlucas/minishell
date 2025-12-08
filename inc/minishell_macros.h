466c661e1c52aa96be6a41a518221645817a0d48
Sara Sin <saracsin26@gmail.com>
2025-12-08 21:36:57 +0100

    update folder structure

---
+++ inc/minishell_macros.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_macros.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssin <ssin@student.42berlin.de>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/05 15:53:52 by ssin              #+#    #+#             */
+/*   Updated: 2025/12/05 16:32:10 by ssin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_MACROS_H
+# define MINISHELL_MACROS_H
+
+/* Metacharacters */
+# define PIPE                "|"
+# define RED_IN              "<"
+# define RED_IN_HD           "<<"
+# define RED_OUT_TRUN        ">"
+# define RED_OUT_APP         ">>"
+# define S_QUOTE             '"'
+# define D_QUOTE             "'"
+# define EXP                 "$"
+
+#endif
