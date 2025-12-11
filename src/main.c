67902d7089a5fd6fac6eadff86741db5cf181e0a
Sara Sin <ssin@student.42berlin.de>
2025-12-11 19:20:54 +0100

    create a specific dir/function to readline

--- src/main.c
+++ src/main.c
@@ -1,44 +1,7 @@
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
 #include "../inc/minishell.h"
-
-static void print_tokens(t_token *tokens)
-{
-	t_token	*current = tokens;
-
-	while (current)
-	{
-		printf("[%s]", current->value);
-		if (current->next)
-			printf("->");
-		current = current->next;
-	}
-	printf("->NULL\n");
-}
 
 int	main(void)
 {
-	char	*prompt;
-	t_token	*tokens;
-
-	while (1)
-	{
-		prompt = readline("$[ 🛸 ]>");
-		if (!prompt)
-		{
-			printf("exit\n");
-			break;
-		}
-		if (!*prompt)
-		{
-			free(prompt);
-			continue ;
-		}
-		tokens = lexer(prompt);
-		print_tokens(tokens);
-		token_clear(&tokens);
-		free(prompt);
-	}
+	readline_loop();
 	return (0);
 }
