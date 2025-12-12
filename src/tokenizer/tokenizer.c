468c9a0fbb924e01f677b78ed0f8a271bfbbfa14
Sara Sin <ssin@student.42berlin.de>
2025-12-12 18:38:16 +0100

    tokenizer refactor

--- src/tokenizer/tokenizer.c
+++ src/tokenizer/tokenizer.c
@@ -1,53 +1,86 @@
-#include "../inc/minishell.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssin <ssin@student.42berlin.de>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/12 18:08:03 by ssin              #+#    #+#             */
+/*   Updated: 2025/12/12 18:08:05 by ssin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-t_token	*lexer(char *line)
+#include "../../inc/minishell.h"
+
+char	*get_word(char *input)
 {
-	t_token	*tokens = NULL;
 	char	*start;
 	int		len;
 	char	*word;
 
-	while (*line)
+	start = input;
+	while (*input && !ft_isspace(*input) && *input != '|' && *input != '<'
+		&& *input != '>' && *input != '"' && *input != '\'')
+		input++;
+	len = input - start;
+	word = ft_substr(start, 0, len);
+	return (word);
+}
+
+char	*get_token_metachar(char *input)
+{
+	if (*input == PIPE)
+		return ("|");
+	else if (*input == RED_IN)
 	{
-		while (ft_isspace(*line))
-			line++;
-		if (!*line)
-			break ;
-		if (*line == '|')
-		{
-			token_add_back(&tokens, new_token("|", TOKEN_PIPE));
-			line++;
-		}
-		else if (*line == '<' && *(line + 1) == '<')
-		{
-			token_add_back(&tokens, new_token("<<", TOKEN_HEREDOC));
-			line +=2;
-		}
-		else if (*line == '<')
-		{
-			token_add_back(&tokens, new_token("<", TOKEN_REDIR_IN));
-			line++;
-		}
-		else if (*line == '>' && *(line + 1) == '>')
-		{
-			token_add_back(&tokens, new_token(">>", TOKEN_REDIR_APPEND));
-			line +=2;
-		}
-		else if (*line == '>')
-		{
-			token_add_back(&tokens, new_token(">", TOKEN_REDIR_OUT));
-			line++;
-		}
-		else
-		{
-			start = line;
-			while (*line && !ft_isspace(*line) && *line != '|' && *line != '<' && *line != '>')
-				line++;
-			len = line - start;
-			word = ft_substr(start, 0, len);
-			token_add_back(&tokens, new_token(word, TOKEN_WORD));
-			free(word);
-		}
+		if (*(input + 1) == RED_IN)
+			return ("<<");
+		return ("<");
 	}
-	return (tokens);
+	else if (*input == RED_OUT)
+	{
+		if (*(input + 1) == RED_OUT)
+			return (">>");
+		return (">");
+	}
+	else if (*input == S_QUOTE)
+		return ("'");
+	else if (*input == D_QUOTE)
+		return ("\"");
+	else if (*input == EXP)
+		return ("$");
+	else
+		return (get_word(input));
+}
+
+t_token_type	get_type(char input)
+{
+	(void)input;
+	return (TOKEN_PIPE);
+}
+
+t_token	*lexer(char *input)
+{
+	t_token			*tokens_list;
+	char			*metacharacter;
+	int				len;
+	t_token_type	type;
+
+	len = 0;
+	tokens_list = NULL;
+	while (*input)
+	{
+		while (ft_isspace(*input))
+			input++;
+		if (!*input)
+			break ;
+		metacharacter = get_token_metachar(input);
+		len = ft_strlen(metacharacter);
+		input += len;
+		type = get_type(*input);
+		token_add_back(&tokens_list, new_token(metacharacter, type));
+		if (type == TOKEN_WORD)
+			free(metacharacter);
+	}
+	return (tokens_list);
 }
