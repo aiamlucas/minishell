486e2627d903f2458382c44dec9a71d9d6b0a983
Lucas Maia <lucasm@posteo.de>
2026-03-18 21:14:50 +0100

    chore: remove debug print from readline loop

--- src/readline/process_input.c
+++ src/readline/process_input.c
@@ -6,7 +6,7 @@
 /*   By: lbueno-m <lbueno-m@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 14:29:01 by lbueno-m          #+#    #+#             */
-/*   Updated: 2026/03/11 15:53:34 by ssin             ###   ########.fr       */
+/*   Updated: 2026/03/18 19:59:12 by lbueno-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,17 @@ int	process_input(char *input, t_data *data)
 	int			exit_code;
 
 	data->tokens = lexer(input);
-	printf("tokens\n");
-	print_tokens(data->tokens);
-	printf("\n after expansion: \n");
 	if (!expand_tokens(data->tokens, data->internal_env, data->last_exit))
 	{
 		token_clear(&data->tokens);
 		return (1);
 	}
-	print_tokens(data->tokens);
-	printf("\n after quote removal: \n");
 	if (!remove_quotes(data->tokens))
 	{
 		token_clear(&data->tokens);
 		return (1);
 	}
-	print_tokens(data->tokens);
 	data->commands = parser(data->tokens);
-	// print_commands(data->commands); // for debugging
 	token_clear(&data->tokens);
 	if (data->commands)
 	{
@@ -42,7 +35,6 @@ int	process_input(char *input, t_data *data)
 		if (exit_code == 0 && !g_signal_received)
 			exit_code = execute_command(data);
 	}
-	//exit_code = execute_command(data);
 	command_clear(&data->commands);
 	if (check_signal())
 		exit_code = get_signal_exit_code();
